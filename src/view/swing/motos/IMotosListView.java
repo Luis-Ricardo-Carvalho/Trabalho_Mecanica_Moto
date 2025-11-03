5bd62d8e327ec03d30e47d82a9f71aa27b0b4f9a
Luís <luisricardocarvalholin@gmail.com>
2025-11-03 07:11:33 -0300

    Delete src directory

--- src/view/swing/motos/IMotosListView.java
+++
@@ -1,9 +0,0 @@
-package view.swing.motos;
-
-import java.util.List;
-import model.Motos;
-
-public interface IMotosListView {
-    void setMotoList(List<Motos> motos);
-    void showMessage(String msg);
-}
--- src/view/swing/pecas/IPecasListView.java
+++
@@ -1,9 +0,0 @@
-package view.swing.pecas;
-
-import java.util.List;
-import model.Pecas;
-
-public interface IPecasListView {
-    void setPecaList(List<Pecas> pecas);
-    void showMessage(String msg);
-}
